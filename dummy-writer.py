3d2a41dff39be3db812a4841a3f7b5c87c1c01aa
zlkn <zolkin.u.a@gmail.com>
2023-11-18 15:49:29 +0200

    setup local env

--- dummy-writer.py
+++ dummy-writer.py
@@ -1,66 +1,74 @@
 import time
 import psycopg2
-from loremipsum import get_sentences
+import loremipsum
+import os
 
-DATABASE = 'dummy'
-PORT = 5432
-USER = 'postgres'
-HOST = 'localhost'
-TABLE = 'data'
+# Fetching environment variables
+DATABASE = os.getenv('DATABASE', 'dummy')
+PORT = int(os.getenv('PORT', 5432))
+USER = os.getenv('USER', 'dummy')
+PASSWORD = os.getenv('PASSWORD', 'postgres')
+HOST = os.getenv('HOST', 'localhost')
+TABLE = os.getenv('TABLE', 'data')
+SLEEP = int(os.getenv('SLEEP', 1))
 
 
-def insert(data: list):
-    sql = """INSERT INTO {table}(data) VALUES(%s) RETURNING id;""".format(table=TABLE)
+def insert(query: str):
     conn = None
-    vendor_id = None
+    id = None
     try:
-        conn = psycopg2.connect(database=DATABASE, port=PORT,  user=USER, host=HOST)
+        print(f'query={query}')
+
+        print(f'connection: database={DATABASE}, \
+            port={PORT}, \
+            user={USER}, \
+            password={PASSWORD}, \
+            host={HOST}')
+
         cur = conn.cursor()
-        for line in data:
-            cur.execute(sql, (line,))
-        vendor_id = cur.fetchone()[0]
-        conn.commit()
+        conn.autocommit = True
+        cur.execute(query)
+        id = cur.fetchone()[0]
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+
     finally:
         if conn is not None:
             conn.close()
-    return vendor_id
+    return id
+
 
 def create_database():
     sql = """CREATE DATABASE {database}""".format(database=DATABASE)
-    conn = None
-    try:
-        conn = psycopg2.connect(port=PORT, user=USER, host=HOST)
-        cur = conn.cursor()
-        conn.autocommit = True
-        cur.execute(sql)
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    insert(sql)
+
 
 def create_table():
-    sql = """CREATE TABLE IF NOT EXISTS {table}  (id SERIAL PRIMARY KEY, data VARCHAR(200))""".format(table=TABLE)
-    conn = None
-    try:
-        conn = psycopg2.connect(database=DATABASE, port=PORT,  user=USER, host=HOST)
-        cur = conn.cursor()
-        conn.autocommit = True
-        cur.execute(sql)
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    sql = """
+        CREATE TABLE IF NOT EXISTS
+            {table}  (
+                id SERIAL PRIMARY KEY,
+                data VARCHAR(200)
+                )
+        """.format(table=TABLE)
+    insert(sql)
+
+
+def insert_random_string():
+    sentence = loremipsum.get_sentence()
+    print(sentence)
+    sql = """
+        INSERT INTO {table}(data)
+        VALUES ("{string}")
+        RETURNING id;
+        """.format(table=TABLE, string=sentence)
+    return insert(sql)
+
 
 if __name__ == '__main__':
     create_database()
     create_table()
     while True:
-        print(insert(get_sentences(1)))
-        time.sleep(1)
+        print(insert_random_string())
+        time.sleep(SLEEP)

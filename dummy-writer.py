34658adaf93e1722383e611bfc6fa1fd82760df4
zlkn <zolkin.u.a@gmail.com>
2023-11-19 00:01:22 +0200

    Add retry

--- dummy-writer.py
+++ dummy-writer.py
@@ -4,47 +4,56 @@ import loremipsum
 import os
 
 # Fetching environment variables
-DATABASE = os.getenv('DATABASE', 'dummy')
-PORT = int(os.getenv('PORT', 5432))
-USER = os.getenv('USER', 'dummy')
-PASSWORD = os.getenv('PASSWORD', 'postgres')
-HOST = os.getenv('HOST', 'localhost')
-TABLE = os.getenv('TABLE', 'data')
+DATABASE = os.getenv('POSTGRES_DATABASE', 'dummy')
+PORT = int(os.getenv('POSTGRES_PORT', 5432))
+USER = os.getenv('POSTGRES_USER', 'dummy')
+PASSWORD = os.getenv('POSTGRES_PASSWORD', 'postgres')
+HOST = os.getenv('POSTGRES_HOST', 'localhost')
+TABLE = os.getenv('POSTGRES_TABLE', 'data')
 SLEEP = int(os.getenv('SLEEP', 1))
 
 
 def insert(query: str):
-    conn = None
     id = None
+    conn = None
     try:
-        print(f'query={query}')
-
-        print(f'connection: database={DATABASE}, \
-            port={PORT}, \
-            user={USER}, \
-            password={PASSWORD}, \
-            host={HOST}')
-
+        conn = psycopg2.connect(port=PORT, host=HOST,
+                                database=DATABASE,
+                                user=USER, password=PASSWORD,
+                                )
         cur = conn.cursor()
         conn.autocommit = True
         cur.execute(query)
         id = cur.fetchone()[0]
         cur.close()
+
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
     finally:
         if conn is not None:
             conn.close()
+
     return id
 
 
-def create_database():
+def retry_insert(query: str, max_retries=5, delay=2):
+    id = None
+
+    for retry in range(max_retries):
+        id = insert(query)
+        if id is not None:
+            break
+        else:
+            time.sleep(delay)
+
+    return id
+
+
+def init():
     sql = """CREATE DATABASE {database}""".format(database=DATABASE)
     insert(sql)
 
-
-def create_table():
     sql = """
         CREATE TABLE IF NOT EXISTS
             {table}  (
@@ -56,19 +65,18 @@ def create_table():
 
 
 def insert_random_string():
-    sentence = loremipsum.get_sentence()
-    print(sentence)
+    sentence = loremipsum.get_sentence().replace("'", "\"")
     sql = """
         INSERT INTO {table}(data)
-        VALUES ("{string}")
+        VALUES ('{sentence}')
         RETURNING id;
-        """.format(table=TABLE, string=sentence)
+        """.format(table=TABLE, sentence=sentence)
+
     return insert(sql)
 
 
 if __name__ == '__main__':
-    create_database()
-    create_table()
+    init()
     while True:
         print(insert_random_string())
         time.sleep(SLEEP)
